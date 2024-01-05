773538f900f81cc0e80cffc2e26bc2fc4c5cb772
root <root@localhost.localdomain>
2024-01-05 14:32:52 -0500

    print yaml ready CA content

--- get-cert-details.sh
+++ get-cert-details.sh
@@ -1,3 +1,7 @@
 docker cp es-es01-1:/usr/share/elasticsearch/config/certs/ca/ca.crt /tmp/.
-openssl x509 -fingerprint -sha256 -noout -in /tmp/ca.crt | awk -F"=" {' print $2 '} | sed s/://g
 cat /tmp/ca.crt
+echo 'ssl:';
+echo '  certificate_authorities:';
+echo '  - |';
+cat /tmp/ca.crt | sed 's/^/    /'
+openssl x509 -fingerprint -sha256 -noout -in /tmp/ca.crt | awk -F"=" {' print $2 '} | sed s/://g
